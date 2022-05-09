5f152224d00428d810ddb8a8c78b72e94ef569e5
Andrew O'Rourke <andreworourke84@gmail.com>
2022-05-09 21:27:51 +1000

    Merge sort Class initial code implementation

--- src/ds/sorter/MergeSorter.java
+++ src/ds/sorter/MergeSorter.java
@@ -6,6 +6,46 @@ public class MergeSorter<E extends Comparable<E>> implements Sorter<E> {
 
 	@Override
 	public List<E> sort(List<E> input) {
-		return input;
+
+		E
 	}
+
+	public void mergeSort(E[] a, int n) {
+		if (n < 2) {
+			return;
+		}
+		int mid = n / 2;
+		E[] l = new E[mid];
+		E[] r = new E[n - mid];
+
+		for (int i = 0; i < mid; i++) {
+			l[i] = a[i];
+		}
+		for (int i = mid; i < n; i++) {
+			r[i - mid] = a[i];
+		}
+		mergeSort(l, mid);
+		mergeSort(r, n - mid);
+
+		merge(a, l, r, mid, n - mid);
+	}
+
+	public void merge(E[] a, E[] l, E[] r, int left, int right) {
+
+		int i = 0, j = 0, k = 0;
+		while (i < left && j < right) {
+			if (l[i] <= r[j]) {
+				a[k++] = l[i++];
+			} else {
+				a[k++] = r[j++];
+			}
+		}
+		while (i < left) {
+			a[k++] = l[i++];
+		}
+		while (j < right) {
+			a[k++] = r[j++];
+		}
+	}
+
 }
