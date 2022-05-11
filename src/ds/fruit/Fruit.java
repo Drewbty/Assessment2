7d564530f6fafadb186cea2b142352068222e3ed
Andrew O'Rourke <andreworourke84@gmail.com>
2022-05-11 22:23:21 +1000

    Added method javadoc comments and inline comments for Fruit Class

--- src/ds/fruit/Fruit.java
+++ src/ds/fruit/Fruit.java
@@ -2,6 +2,12 @@ package ds.fruit;
 
 import java.util.Objects;
 
+/**
+ * 
+ * @author AndrewORourke
+ * Unisa student username: oroaj002
+ */
+
 public class Fruit implements java.lang.Comparable<Fruit> {
 
 	public enum TYPE {
@@ -13,15 +19,26 @@ public class Fruit implements java.lang.Comparable<Fruit> {
 	private double weight;
 	private double ripeness;
 
+	
+	/**
+	 * Constructor for FRuit Object
+	 * @param myType - type of Fruit
+	 * @param weight - weight of Fruit
+	 * @param ripeness - ripeness of Fruit
+	 * @throws InvalidFruitWeightException
+	 * @throws InvalidFruitRipenessException
+	 */
 	public Fruit(final TYPE myType, final double weight, final double ripeness) throws InvalidFruitWeightException, InvalidFruitRipenessException {
 
 		this.type = myType;
+		//if weight is negative
 		if (weight <= 0) {
 			throw new InvalidFruitWeightException("Invalid weight: " + weight + ". Weight must be positive");
 		} else {
 			this.weight = weight;
 		}
 
+		//if ripeness is not between 0 and 1
 		if (ripeness < 0 || ripeness > 1) {
 			throw new InvalidFruitRipenessException("Invalid ripeness: " + ripeness + ". Ripeness must be between 0 and 1");
 		} else {
@@ -29,6 +46,9 @@ public class Fruit implements java.lang.Comparable<Fruit> {
 		}
 
 	}
+	/**
+	 * ToString method for Fruit object
+	 */
 
 	public String toString() {
 
@@ -59,25 +79,26 @@ public class Fruit implements java.lang.Comparable<Fruit> {
 	
 	
 
-	/**
-	 * @return the type
-	 */
 	public TYPE getType() {
 		return type;
 	}
 
-	/**
-	 * @param type the type to set
-	 */
+
 	public void setType(TYPE type) {
 		this.type = type;
 	}
 
+	/**
+	 * Override hashcode method
+	 */
 	@Override
 	public int hashCode() {
 		return Objects.hash(ripeness, weight);
 	}
 
+	/**
+	 * Override equals method
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -94,36 +115,49 @@ public class Fruit implements java.lang.Comparable<Fruit> {
 				&& Double.doubleToLongBits(weight) == Double.doubleToLongBits(other.weight);
 	}
 
+	//set the minimum difference in weight, whereby the weight is ignored
 	final double DELTA_DIFF = 0.05;
 	
 	@Override
 	public int compareTo(Fruit o) {
+		//if both pears, compare the ripeness to separate them
 		if (getType() == TYPE.PEAR) {
 			if (o.getType() == TYPE.PEAR) {
 				return compareRipeness(o);
 			}
+			//otherwise pear is always greater
 			return 1;
 		}
 		
+		
 		if (getType() == TYPE.BANANA) {
+			//if both bananas, compare ripeness to separate them
 			if (o.getType() == TYPE.BANANA) {
 				return compareRipeness(o);
 			}else {
+				//otherwise, bananas are always lesser
 				return -1;
 			}
 		}
 		if (getType() == TYPE.GRAPE || getType() == TYPE.APPLE) {
+			//if grape or apple is compared to pear, they are lesser
 			if (o.getType() == TYPE.PEAR) {
 				return -1;
+				//if grape or apple is compared to banana, they are both greater
 			}else if (o.getType() == TYPE.BANANA) {
 				return 1;
 				
 			}else {
+				
+				//otherwise we are comparing grapes/apples to grapes/apples
 				int comparisonWeight = compareWeight(o);
+				//we compare the weight, if they're not equal (including DELTA_DIFF)
 				if (comparisonWeight != 0) {
+					//return the weight
 					return comparisonWeight;
 				}
 				
+				//otherwise, compare ripeness to tie break
 				return compareRipeness(o);
 			}
 		}
@@ -131,7 +165,14 @@ public class Fruit implements java.lang.Comparable<Fruit> {
 		return 0;
 	}
 
+	/**
+	 * Method to compare the weight of 2 Fruit Objects. If the objects are within .05kg in weight
+	 * They are treated as the same weight for comparison. 
+	 * @param o Fruit object to compare to
+	 * @return 0 (equal), -1 (lesser), 1 (greater)
+	 */
 	private int compareWeight(Fruit o) {
+		//get absolute difference in weight
 		if (Math.abs(getWeight() - o.getWeight()) <= DELTA_DIFF ) {
 			return 0;
 		}else if (getWeight() > o.getWeight()) {
@@ -142,6 +183,11 @@ public class Fruit implements java.lang.Comparable<Fruit> {
 		}
 	}
 	
+	/**
+	 * Method to compare the ripeness of 2 Fruit Objects.
+	 * @param o Fruit object to compare to
+	 * @return 0 (equal), -1 (lesser), 1 (greater)
+	 */
 
 	private int compareRipeness(Fruit o) {
 		if (getRipeness() == o.getRipeness()) {
