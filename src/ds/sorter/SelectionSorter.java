f4c60389950e3f73cd8ef5589fc4ad7d0e8b26f7
Andrew O'Rourke <andreworourke84@gmail.com>
2022-05-11 20:18:24 +1000

    Implemented selectionSorter Class and comments

--- src/ds/sorter/SelectionSorter.java
+++ src/ds/sorter/SelectionSorter.java
@@ -1,12 +1,35 @@
 package ds.sorter;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class SelectionSorter<E extends Comparable<E>> implements Sorter<E> {
 
 	@Override
 	public List<E> sort(List<E> input) {
-		return null;
+		Object[] items = input.toArray();
+		int n = items.length;
+		 
+        // Move outer limit of unsorted subarray
+        for (int i = 0; i < n-1; i++)
+        {
+            // Find minimum element in unsorted array
+            int minimum = i;
+            for (int j = i+1; j < n; j++)
+                if (((E)items[j]).compareTo((E) items[minimum]) <= 0) {
+                	minimum = j;        	
+                }
+ 
+            // Swap the found minimum element with the first element of the outer limit
+            Object temp = items[minimum];
+            items[minimum] = items[i];
+            items[i] = temp;
+        }
+        
+        //cast back as list
+        return (List<E>) Arrays.asList(items);
    	}
+	
+	
 
 }
